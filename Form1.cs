302fecd94a1a59a1f786201f41b592c3cdde7840
quy2222008 <voquocquy23032022@gmail.com>
2024-06-17 14:14:58 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_HinhChuNhat_Class
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Opacity = 0.5;
+        }
+
+        private void btnDientich_Click(object sender, EventArgs e)
+        {
+            hinhchunhat hcn = new hinhchunhat();
+            hcn.chieucao = double.Parse(txtCao.Text);
+            hcn.chieurong= double.Parse(txtRong.Text);
+            txtketqua.Text= hcn.tinhdiendich().ToString();
+           
+        }
+
+        private void btnChuvi_Click(object sender, EventArgs e)
+        {
+            hinhchunhat hcn = new hinhchunhat();
+            hcn.chieucao = double.Parse(txtCao.Text);
+            hcn.chieurong = double.Parse(txtRong.Text);
+            txtketqua.Text = hcn.tinhchuvi().ToString();
+        }
+    }
+}
